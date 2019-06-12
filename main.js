208cb9a2e683a05452be9453d3a4189be4d9ac61
Marcelo Jorge <marcelojorgeschultz@hotmail.com>
2019-06-12 20:03:15 -0300

    Update no bot!
    Terminado o comando de server info

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ client.on("message", async message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const comando = args.shift().toLowerCase();
     const block = "`";
+    const errorblock = '```';
 
     if(message.content.startsWith(prefix + `serverinfo`)) {
         let region = {
@@ -35,20 +36,19 @@ client.on("message", async message => {
             "russia": ":flag_ru: Russia",
             "southafrica": ":flag_za:  South Africa"
         };
-    
-        
+        let server_icon = message.guild.iconURL || "https://cdn.discordapp.com/emojis/588492643063955470.png?v=1"
         const embed = new Discord.RichEmbed()
         .setTitle(`**Server info**`)
         .setColor(`#bc42f4`)
-        .setAuthor(`${message.guild.name}`, `${message.guild.iconURL}`)
+        .setAuthor(`${message.guild.name}`, `${server_icon}`)
         .addField(`**<:member:588187238261587997>Total de membros**`, `**${block}${message.guild.memberCount}${block} membros**`, true)
         .addField(`**<:chats:588187612288909312>Numero de canais**`, `**${block}${message.guild.channels.size}${block} canais**`, true)
         .addField(`**<:cargos:588189237371994112>Numero de cargos**`, `**${block}${message.guild.roles.size}${block} cargos**`, true)
         .addField(`<:region:588191122384486414>**Região**`, `${region[message.guild.region]}`, true)
-        message.channel.send(embed)
+        .addField(`**<:people:588502135185080335>Dono**`, `${message.guild.owner}`, true)
+        .setFooter(`Athisum`, `${message.client.user.avatarURL}`)
+        message.channel.send(embed) .catch(err => message.channel.send(`**<:error:588492643063955470>Um erro aconteceu!**\n**${errorblock}js\n${err}${errorblock}**`))
     }
-
-
 })
 
-client.login(`${config.token}`);
+client.login(`${config.token}`); 
